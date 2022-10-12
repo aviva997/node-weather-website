5a877e01856be119c734109583cda5b0c0f8e7d0
aviva997 <73630522+aviva997@users.noreply.github.com>
2022-10-12 17:51:49 +0300

    change in weather.js

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -2,6 +2,7 @@ const request = require('request')
 
 const weather = (cord1,cord2, callback)=>{
     const url = 'http://api.weatherstack.com/current?access_key=44eb30cf1e104d011a04059835ad7a44&query=' + cord1 + ',' + cord2 +'&units=f'
+    console.log(url)
     request({url, json: true},(error, {body})=>{
         if(error){
             callback("Unable to contect to weather service",undefined)
@@ -11,8 +12,7 @@ const weather = (cord1,cord2, callback)=>{
             callback("Unable to find location",undefined)
 
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+'. it is a currently '+body.current.temperature+' out. It feels ' +body.current.feelslike+' degrees out .')
-
+            callback(undefined,'The temerature is -'+body.current.temperature+'. It feels like -'+body.current.feelslike+'. The humidity -'+ body.current.humidity)
         }
     })
 }
